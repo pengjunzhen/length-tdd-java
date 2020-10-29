e7f66f231d3eeb2df345959ffccbfd85967df394
pengjunzhen <pengjunzhenwork@163.com>
2020-10-29 22:26:32 +0800

    refactor: inline YARD And INCH with Ctrl + Alt + N

--- src/main/java/Length.java
+++ src/main/java/Length.java
@@ -2,8 +2,8 @@ public class Length {
     private final double value;
     private final String unit;
     public static final String FOOT = "f";
-    private static final String YARD = "yard";
-    private static final String INCH = "inch";
+    public static final String YARD = "yard";
+    public static final String INCH = "inch";
 
     public Length(double value, String unit) {
         this.value = value;
@@ -12,28 +12,26 @@ public class Length {
 
     public Length as(String targetUnit) {
         Length result = this;
-        final String YARD = Length.YARD;
-        final String INCH = Length.INCH;
         if (this.unit.equals(Length.FOOT)) {
-            if (targetUnit.equals(YARD)) {
+            if (targetUnit.equals(Length.YARD)) {
                 result = new Length(this.value / 3, targetUnit);
-            } else if (targetUnit.equals(INCH)) {
+            } else if (targetUnit.equals(Length.INCH)) {
                 result = new Length(this.value * 12, targetUnit);
             }
         }
 
-        if (this.unit.equals(YARD)) {
-            if (targetUnit.equals(INCH)) {
+        if (this.unit.equals(Length.YARD)) {
+            if (targetUnit.equals(Length.INCH)) {
                 result = new Length(this.value * 36, targetUnit);
             } else if (targetUnit.equals(Length.FOOT)){
                 result = new Length(this.value * 3, targetUnit);
             }
         }
 
-        if (this.unit.equals(INCH)) {
+        if (this.unit.equals(Length.INCH)) {
             if (targetUnit.equals(Length.FOOT)) {
                 result = new Length(this.value / 12, targetUnit);
-            } else if (targetUnit.equals(YARD)) {
+            } else if (targetUnit.equals(Length.YARD)) {
                 result = new Length(this.value / 36, targetUnit);
             }
         }
